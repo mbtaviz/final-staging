5b7c90e13af64d48ef4130b9ca1e5b82719f0be7
Mike Barry <msb5014@gmail.com>
2014-04-25 06:54:34 -0400

    Built mbtaviz from commit ffa4702 on branch master

--- scripts/turnstile.js
+++ scripts/turnstile.js
@@ -13,8 +13,8 @@ VIZ.requiresData([
   var hourWidth = 3;
   var hourHeight = 8;
   var dayWidth = 24 * hourWidth;
-  var totalWidth = dayWidth * 8;
-  var labelWidth = 200;
+  var totalWidth = dayWidth * 7.5 + 10;
+  var labelWidth = 150;
   var sectionHeight = hourHeight * (1 + 4 * 2) + 40;
 
   var stopMargin = {top: 15,right: 0,bottom: 0,left: 0}
@@ -56,8 +56,48 @@ VIZ.requiresData([
       .html(function(d) { return getTextForRollover(d, d3.select(this).classed('entrances') ? 'entrances' : 'exits'); });
   svg.call(tip);
 
+  var HIGHLIGHTS = {
+    'holidays': [
+      [3, 1]
+    ],
+    'snow': [
+      [1, 3],
+      [2, 4]
+    ]
+  }
+  var highlightGroups = total.selectAll('.highlight-group')
+      .data([2, 7])
+      .enter()
+    .append('g')
+      .attr('class', function (d) { return 'highlight-group num' + d; })
+      .attr('transform', function (d) { return 'translate(' + (labelWidth + gridMargin.left + 15) + ',' + (d * (dayMargin.bottom + hourHeight) - 6) + ')'; });
   d3.selectAll('.section-people .highlight').on('click', function (d) {
     d3.event.preventDefault();
+  })
+  .on('mouseover', function (d) {
+    var highlight = d3.select(this).attr('data-highlight');
+    var list = HIGHLIGHTS[highlight];
+    highlightGroups.selectAll('rect.highlight')
+        .data(list)
+        .enter()
+      .append('rect')
+        .attr('class', 'highlight')
+        .attr('width', dayWidth - 7)
+        .attr('height', hourHeight + 2)
+        .attr('x', function (d) { return (dayWidth + dayMargin.right) * d[1]; })
+        .attr('y', function (d) { return (hourHeight + dayMargin.bottom) * d[0]; })
+    total.selectAll('.num2').selectAll('line.highlight')
+        .data(list)
+        .enter()
+      .append('line')
+        .attr('class', 'highlight')
+        .attr('x1', function (d) { return (dayWidth + dayMargin.right) * d[1]; })
+        .attr('x2', function (d) { return (dayWidth + dayMargin.right) * d[1]; })
+        .attr('y1', function (d) { return (hourHeight + dayMargin.bottom) * d[0]; })
+        .attr('y2', function (d) { return (hourHeight + dayMargin.bottom) * d[0] + 5 * (dayMargin.bottom + hourHeight); });
+  })
+  .on('mouseout', function (d) {
+    total.selectAll('.highlight').remove();
   });
     
   function drawStop(container, i, name, stopData, aggs) {
@@ -153,13 +193,13 @@ VIZ.requiresData([
 
       svg.append('text')
         .attr('class', 'groupLabel')
-        .attr('transform', 'translate(' + (totalWidth - 35) + ',' + (5) + ')rotate(90)')
+        .attr('transform', 'translate(' + (totalWidth - 10) + ',' + (5) + ')rotate(90)')
         .text('entrances')
         .style('text-anchor', 'beginning');
 
       svg.append('text')
         .attr('class', 'groupLabel')
-        .attr('transform', 'translate(' + (totalWidth - 35) + ',' + (hourHeight * 8.5) + ')rotate(90)')
+        .attr('transform', 'translate(' + (totalWidth - 10) + ',' + (hourHeight * 8.5) + ')rotate(90)')
         .text('exits')
         .style('text-anchor', 'beginning');
 
